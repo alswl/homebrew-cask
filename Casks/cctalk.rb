8c981e06ebd2915ce07f53a1f6d25d7be85e7050
alswl <alswlx@gmail.com>
2017-07-03 11:15:36 +0800

    Update cctalk to 0.8.3-260,2017-06-30.13.25

--- Casks/cctalk.rb
+++ Casks/cctalk.rb
@@ -1,11 +1,11 @@
 cask 'cctalk' do
-  version '0.8.2-242,2017-06-14.21.26'
-  sha256 'a3afdfb1e6db9f512c4a154001d200aa224fe974ee4c9f76acccf9711018130f'
+  version '0.8.3-260,2017-06-30.13.25'
+  sha256 '37b79b64f66b6dc7fc07db46bef1ca4dd4a20cf43fdcc82504312fedfda43e78'
 
   # f1.ct.hjfile.cn was verified as official when first introduced to the cask
-  url "http://f1.ct.hjfile.cn/api/AutoUpdate/newupdate/out/mac/cctalk/archive/#{version.before_comma.hyphens_to_dots}/CCtalk-#{version.before_comma}-hujiang-#{version.after_comma}.dmg"
+  url "http://f1.ct.hjfile.cn/api/AutoUpdate/newupdate/out/mac/cctalk/archive/#{version.before_comma.hyphens_to_dots}/CCtalk-#{version.before_comma}-xianghu-#{version.after_comma}.dmg"
   appcast 'http://f1.ct.hjfile.cn/api/AutoUpdate/newupdate/out/mac/cctalk/update/info.xml',
-          checkpoint: '2c6841769e9c743903b26117fb5308b9fe9efc5c41d431a4d43a905edd1e24a4'
+          checkpoint: '40b075ec95fe25a218ac13da74ebfb96780f46c26148218a4db03fac1749626f'
   name 'CCTalk'
   homepage 'https://www.cctalk.com/download/'
 
